63f46befaaa2d653adf75919746c86b04fb31cbb
Malavika Manohar M <150352076+malavikamanohar@users.noreply.github.com>
2025-11-11 16:19:36 +0530

    Update carousel.tsx

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -11,6 +11,8 @@ type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
 type CarouselOptions = UseCarouselParameters[0];
 type CarouselPlugin = UseCarouselParameters[1];
 
+
+
 type CarouselProps = {
   opts?: CarouselOptions;
   plugins?: CarouselPlugin;
