87c6a68d6798216e1fba9bb0af363be0ac3935df
Malavika Manohar M <150352076+malavikamanohar@users.noreply.github.com>
2025-11-11 16:29:02 +0530

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -10,6 +10,7 @@ const SelectGroup = SelectPrimitive.Group;
 
 const SelectValue = SelectPrimitive.Value;
 
+
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
