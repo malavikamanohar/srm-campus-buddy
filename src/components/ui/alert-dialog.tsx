ad375eb4584a324edf4e443fed298c59452765d1
Malavika Manohar M <150352076+malavikamanohar@users.noreply.github.com>
2025-11-11 16:15:13 +0530

    Add missing imports in alert-dialog.tsx

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -4,6 +4,8 @@ import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
+
+
 const AlertDialog = AlertDialogPrimitive.Root;
 
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
