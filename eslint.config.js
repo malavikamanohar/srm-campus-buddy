c468f7b3d441410b39fa65422f169c93e9af7600
Malavika Manohar M <150352076+malavikamanohar@users.noreply.github.com>
2025-11-11 15:59:52 +0530

    Add react-refresh plugin to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,9 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+
+
+    
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
