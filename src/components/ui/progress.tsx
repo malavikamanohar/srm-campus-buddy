b665d5f9b81ce2b9c494540aa97546dabaf71110
Malavika Manohar M <150352076+malavikamanohar@users.noreply.github.com>
2025-11-11 16:28:12 +0530

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -17,6 +17,7 @@ const Progress = React.forwardRef<
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
+  
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
